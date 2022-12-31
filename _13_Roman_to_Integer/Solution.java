9ef8c240b7eaccaccc5072d257db897605a39fa7
Burak <75482610+burakcantokses@users.noreply.github.com>
2023-01-01 02:56:52 +0300

    Example 3 hatalı çözüm bekleniyor.  https://leetcode.com/problems/roman-to-integer/

---
+++ _13_Roman_to_Integer/Solution.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(romanToInt("MCMXCIV"));
+    }
+    public static int romanToInt(String s) {
+        int number = 0;
+        String key = "";
+        HashMap<String,Integer> romans = new HashMap<String, Integer>();
+        {
+            romans.put("I", 1);
+            romans.put("V", 5);
+            romans.put("X", 10);
+            romans.put("L", 50);
+            romans.put("C", 100);
+            romans.put("D", 500);
+            romans.put("M", 1000);
+        }
+        List<String> checkedValue = new ArrayList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            key = String.valueOf(s.charAt(i));
+            if (i == 0) {checkedValue.add(key);}
+            
+
+            number += romans.get(key);
+        }
+        return number;
+    }
+}
