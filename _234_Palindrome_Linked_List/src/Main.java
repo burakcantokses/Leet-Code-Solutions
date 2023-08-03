4bcf1d55de8cbd7a9999e0ceb0209545d8d5a05d
Burak <75482610+burakcantokses@users.noreply.github.com>
2023-08-03 21:31:51 +0300

    20m

---
+++ _234_Palindrome_Linked_List/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(2);
+        ListNode node4 = new ListNode(1);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        System.out.println(isPalindrome(node1)); // true
+
+    }
+
+    public static boolean isPalindrome(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        ListNode current = head;
+        while (current != null) {
+            list.add(current.val);
+            current = current.next;
+        }
+        int right = list.size()-1;
+        for (Integer left : list) {
+            if (!list.get(right).equals(left)) {
+                return false;
+            }
+            right--;
+        }
+        return true;
+    }
+}
