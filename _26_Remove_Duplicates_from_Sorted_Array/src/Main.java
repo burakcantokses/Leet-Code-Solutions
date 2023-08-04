c8aaf9342f2ae921e1569e4e270da606b06132de
Burak <75482610+burakcantokses@users.noreply.github.com>
2023-08-04 21:31:00 +0300

    10m

---
+++ _26_Remove_Duplicates_from_Sorted_Array/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1,1,2};
+        System.out.println(removeDuplicates(nums));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int k = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+
+}
